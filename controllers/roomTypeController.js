8e3ab7aa59ef6c62dd114017223c8ec7b5429587
Christopher Ric Coelho Saar <cristophersaar@gmail.com>
2023-11-25 16:12:14 -0500

    fix(models) - fixing wrong paste

--- controllers/roomTypeController.js
+++ controllers/roomTypeController.js
@@ -1,88 +1,101 @@
 const db = require('../models');
 const RoomType = db.RoomType;
 
+
+
 async function createRoomType(req, res) {
-  try {
-    const { typeName, description } = req.body;
-    const roomType = await RoomType.create({
-      typeName,
-      description,
-    });
-    res.status(201).json(roomType);
-  } catch (err) {
-    console.error('Erro no createRoomType:', err, req.body);
-    res.status(500).json({ message: 'Ocorreu um erro interno.' });
-  }
+    try {
+        const { name, description } = req.body;
+        const role = await RoomType.create({
+            name: name,
+            description: description,
+        });
+        res.status(201).json(role);
+
+    } catch (err) {
+        console.error('Erro no createRoomType:', err, req.body);
+        res.status(500).json({ message: 'Ocorreu um erro interno.' });
+    }
 }
 
 async function getAllRoomTypes(req, res) {
-  try {
-    const roomTypes = await RoomType.findAll();
-    res.status(200).json(roomTypes);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+    try {
+
+        const roles = await RoomType.findAll();
+        res.status(200).json(roles);
+    } catch (err) {
+
+        res.status(500).json({ message: err.message });
+    }
 }
 
 async function getRoomTypeById(req, res) {
-  try {
-    const { id } = req.params;
-    if (!id) {
-      res.json({ message: 'Você não passou o id no parâmetro' });
+    try {
+
+        const { id } = req.params;
+        if (!id) {
+            res.json({ message: "Você não passou o id no paramentro" })
+        }
+        const role = await RoomType.findOne({
+            where: { id: id }
+        })
+        res.status(200).json(role);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-    const roomType = await RoomType.findByPk(id);
-    res.status(200).json(roomType);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 }
+
 
 async function updateRoomTypeById(req, res) {
-  try {
-    const id = req.params.id;
-    if (!id) {
-      res.json({ message: 'Você não passou o id no parâmetro' });
+    try {
+
+
+        const id = req.params.id;
+        if (!id) {
+            res.json({ message: "Você não passou o id no paramentro" })
+        }
+
+        const roleUpdate = await RoomType.findByPk(id)
+        if (!roleUpdate) {
+            res.json({ message: 'RoomType não encontrada' })
+
+        }
+        const role = req.body;
+
+        await RoomType.update(role, {
+            where: { id: id }
+        });
+        return res.status(200).json({ message: "RoomType atualizada" });
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-
-    const roomTypeToUpdate = await RoomType.findByPk(id);
-    if (!roomTypeToUpdate) {
-      res.json({ message: 'Tipo de sala não encontrado' });
-    }
-
-    const roomType = req.body;
-
-    await RoomType.update(roomType, {
-      where: { id: id },
-    });
-    return res.status(200).json({ message: 'Tipo de sala atualizado' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 }
+
 
 async function deleteRoomTypeById(req, res) {
-  try {
-    const id = req.params.id;
-    if (!id) {
-      res.json({ message: 'Você não passou o id no parâmetro' });
+    try {
+        const id = req.params.id;
+        if (!id) {
+            res.json({ message: "Você não passou o id no paramentro" })
+        }
+        const role = await RoomType.findByPk(id)
+        if (role) {
+            await role.destroy()
+            res.status(204).json({ message: 'RoomType excluída com sucesso' });
+        } else {
+            res.status(404).json({ message: 'RoomType não encontrada' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-
-    const roomType = await RoomType.findByPk(id);
-    if (roomType) {
-      await roomType.destroy();
-      res.status(204).json({ message: 'Tipo de sala excluído com sucesso' });
-    } else {
-      res.status(404).json({ message: 'Tipo de sala não encontrado' });
-    }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 }
 
+
 module.exports = {
-  createRoomType,
-  getAllRoomTypes,
-  getRoomTypeById,
-  updateRoomTypeById,
-  deleteRoomTypeById,
-};
+    createRoomType,
+    getAllRoomTypes,
+    getRoomTypeById,
+    updateRoomTypeById,
+    deleteRoomTypeById,
+}
