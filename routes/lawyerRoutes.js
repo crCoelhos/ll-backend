15acbd9310897c4e99d88ffde509e4b9683bbcb9
crCoelhosm <cristophersaar@gmail.com>
2024-01-16 22:02:17 -0500

    Update (lawyerRoute) - endpoint

--- routes/lawyerRoutes.js
+++ routes/lawyerRoutes.js
@@ -9,7 +9,7 @@ router.get('/lawyer/teste', (req, res) => {
 });
 
 router.post('/lawyer/create', lawyerController.createUser);
-router.get('/lawyers/', authMiddleware, lawyerController.getAllLawyers);
+router.get('/lawyer/all', authMiddleware, lawyerController.getAllLawyers);
 router.get('/lawyer-user/:id', authMiddleware, lawyerController.getLawyerByUserId);
 router.get('/lawyer/:id', authMiddleware, lawyerController.getLawyerById);
 router.get('/lawyer-expertise/:id', authMiddleware, lawyerController.getAllLawyersByExpertise);
