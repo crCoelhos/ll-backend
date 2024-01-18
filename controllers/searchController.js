eabc0452574ef77fb8b26b9efcdad7d7bfdeb6f3
crCoelhosm <cristophersaar@gmail.com>
2024-01-18 17:10:55 -0500

    Refactor (lawyer search) - query and route for retrieving all lawyers

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -10,9 +10,9 @@ async function multipleFieldLawyerSearch(req, res) {
     const searchString = req.params.searchString;
     try {
         let result = await db.sequelize.query(
-            `SELECT u.id, u.name, u.email, u.phoneNumber, u.CPF, u.birthdate, 
-                    la.OAB, la.riteDate, la.inscriptionType, la.graduateDegree, 
-                    la.description, la.image, la.UF, GROUP_CONCAT(e.name) AS expertiseNames
+            `SELECT MAX(u.id) AS userId, MAX(la.id) AS lawyerId, MAX(u.name) AS name, MAX(u.email) AS email, MAX(u.phoneNumber) AS phoneNumber, MAX(u.CPF) AS CPF, MAX(u.birthdate) AS birthdate, 
+                    MAX(la.OAB) AS OAB, MAX(la.riteDate) AS riteDate, MAX(la.inscriptionType) AS inscriptionType, MAX(la.graduateDegree) AS graduateDegree, 
+                    MAX(la.description) AS description, MAX(la.image) AS image, MAX(la.UF) AS UF, GROUP_CONCAT(e.name) AS expertiseNames
              FROM Users AS u
              LEFT JOIN Lawyers AS la ON u.id = la.userId
              LEFT JOIN LawyerExpertises AS le ON la.id = le.lawyerId
@@ -24,7 +24,8 @@ async function multipleFieldLawyerSearch(req, res) {
                     la.UF LIKE '%${searchString}%' OR
                     e.name LIKE '%${searchString}%')
              AND u.roleId = 3
-             GROUP BY u.id, la.id`,
+             AND u.id = la.userId
+             GROUP BY la.userId`,
             { type: db.sequelize.QueryTypes.SELECT }
         );
 
@@ -50,5 +51,7 @@ async function multipleFieldLawyerSearch(req, res) {
 
 
 
+
 module.exports = {
-    multipleFieldLawyerSearch}
+    multipleFieldLawyerSearch
+}
--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const searchController = require('../controllers/searchController.js');
+const lawyerController = require('../controllers/lawyerController.js');
 
 // teste
 router.get('/search-test/teste', (req, res) => {
     res.status(200).json({ message: 'search teste' });
 });
 
-router.get('/search/:searchString', searchController.multipleFieldLawyerSearch);
+router.get('/search/lawyer/:searchString', searchController.multipleFieldLawyerSearch);
+router.get('/search/lawyer/', lawyerController.getAllLawyers);
 
 
 
