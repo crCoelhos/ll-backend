35748715e0453e3671fd12251a68f1fd3a62c299
Christopher Ric Coelho Saar <cristophersaar@gmail.com>
2024-02-27 16:51:42 -0500

    Refactor routes and add CORS origin logging

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 
-
 const authRoutes = require('./routes/authRoutes');
+const routes = require('./routes');
 
 const app = express();
+
 app.use(express.json());
-
-app.use('/auth', authRoutes);
-const routes = require('./routes')
-
-
-// app.use(cors());
 
 const corsOptions = {
   origin: process.env.CORS_ORIGIN,
@@ -24,8 +18,10 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+console.log('CORS_ORIGIN:', process.env.CORS_ORIGIN);
 
-app.use(routes);
+app.use('/auth', authRoutes);
+app.use('/v1', routes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
