8f8dccead87d5e5ec708619df791412967fd6537
Christopher Ric Coelho Saar <cristophersaar@gmail.com>
2024-02-27 17:01:51 -0500

    Add dotenv configuration and update routes in index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
+const routes = require('./routes')
+
 
 const authRoutes = require('./routes/authRoutes');
-const routes = require('./routes');
 
 const app = express();
-
 app.use(express.json());
+
+app.use('/auth', authRoutes);
+
+
 
 const corsOptions = {
   origin: process.env.CORS_ORIGIN,
@@ -20,10 +25,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 console.log('CORS_ORIGIN:', process.env.CORS_ORIGIN);
 
-app.use('/auth', authRoutes);
-app.use('/v1', routes);
+app.use(routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
