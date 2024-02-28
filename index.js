4a8c237d001fe593c853e2e166e555a782a779e4
Christopher Ric Coelho Saar <cristophersaar@gmail.com>
2024-02-27 22:13:11 -0500

    start message

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 
+app.get('/', (req, res) => {
+  res.send('legaliga api running');
+});
 // const corsOptions = {
 //   origin: 'https://legaliga.vercel.app',
 //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
