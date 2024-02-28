560c8ea45ad27787992c07b284f52512416932b5
Christopher Ric Coelho Saar <cristophersaar@gmail.com>
2024-02-27 19:12:46 -0500

    Update database configuration

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 
+
 const dotenv = require('dotenv');
 dotenv.config({path: './config/.env'});
 
@@ -6,12 +7,12 @@ dotenv.config({path: './config/.env'});
 module.exports = {
   development: {
     use_env_variable: 'DATABASE_URL',
-    port: process.env.DB_PORT,
+    port: process.env.TIDB_PORT,
     jwtSecret: process.env.JWT_SECRET,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.TIDB_USER,
+    password: process.env.TIDB_PASSWORD,
+    database: process.env.TIDB_DATABASE,
+    host: process.env.TIDB_HOST,
     dialect: 'mysql',
     dialectOptions: {
       useUTC: false, 
@@ -22,3 +23,27 @@ module.exports = {
     timezone: '-05:00',
   },
 };
+
+// const dotenv = require('dotenv');
+// dotenv.config({path: './config/.env'});
+
+
+// module.exports = {
+//   development: {
+//     use_env_variable: 'DATABASE_URL',
+//     port: process.env.DB_PORT,
+//     jwtSecret: process.env.JWT_SECRET,
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     dialectOptions: {
+//       useUTC: false, 
+//       ssl: {
+//         rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === 'true',
+//       }
+//     },
+//     timezone: '-05:00',
+//   },
+// };
